a45f2e6ae5427bf03cf2eeffadc55b2531b06e21
Mehmet Şirin Usanmaz <mehmet.usanmaz@turk.net>
2024-08-14 08:05:35 +0300

    added speedinsights package

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
